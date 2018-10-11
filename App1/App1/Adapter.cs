7425e6870d284024ec5f13c62f8c9a600593aeb8
Tehnikumi Opilane <opilane@tptlive.ee>
2018-10-11 09:26:31 +0300

    Listview adapter

---
+++ App1/App1/Adapter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    public class Adapter : BaseAdapter<string>
+    {
+        string[] Items;
+
+        Activity Context;
+
+        public Adapter(Activity context, string[] items) : base()
+        {
+            this.Context = context;
+            this.Items = items;
+        }
+
+        public override string this[int position]
+        {
+            get { return Items[position]; }
+        }
+
+
+
+
+
+        public override int Count { get { return Items.Length; } }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View view = convertView;
+            if (view == null)
+                view = Context.LayoutInflater.Inflate(Resource.Layout.Customprogram, null);
+            view.FindViewById<TextView>(Resource.Id.textView2).Text = Items[position];
+            return view;
+            
+
+            
+        }
+    }
+}
---
+++ App1/App1/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : ListActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            string[] animals = new string[] { "kass", "koer", "elevant", "tiiger", "hiir" };
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.activity_main);
+
+            ListAdapter = new Adapter(this, animals);
+
+            ListView.ItemClick += ListView_Itemclick;
+
+            
+        }
+
+        private void ListView_Itemclick(object sender, AdapterView.ItemClickEventArgs args)
+        {
+            Toast.MakeText(Application, ((TextView)args.View).Text, ToastLength.Long).Show();
+        }
+    }
+}
